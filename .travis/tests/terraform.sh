01f7280d3a43519d1ab2d568a41dca55be69085d
Jean-Philippe Fourès <jean.philippe.foures@gmail.com>
2019-09-04 14:51:17 +0200

    tests: use jaypif token

--- .travis/tests/terraform.sh
+++ .travis/tests/terraform.sh
@@ -19,7 +19,7 @@ export OPENNEBULA_ENDPOINT="http://localhost:2633/RPC2"
 export OPENNEBULA_USERNAME="oneadmin"
 export OPENNEBULA_PASSWORD="opennebula"
 export TF_ACC=1
-export CODECOV_TOKEN="b4d4b95c-bb63-4412-b3aa-6d0cf674d619"
+export CODECOV_TOKEN="04b3a535-ef4b-4eda-8495-63b8ec1c5eb8"
 
 # install dep dependencies manager
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
